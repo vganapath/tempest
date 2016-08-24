f7a6c66cce0ac8ab3dc253b94de08589b82dcb50
Vinoth Ganapathy <vganapathy@juniper.net>
2016-08-24 14:20:17 -0700

    Master branch synced with upstream and support tempest for liberty and mitaka for now

---
+++ contrail/contrail-tempest-init.sh
@@ -0,0 +1,105 @@
+#!/usr/bin/env bash
+
+with_venv=tools/with_venv.sh
+
+export OS_USERNAME=${OS_USERNAME:-admin}
+export OS_PASSWORD=${OS_PASSWORD:-contrail123}
+export OS_TENANT_NAME=${OS_TENANT_NAME:-admin}
+export OS_NO_CACHE=1
+
+export TEMPEST_DIR=${TEMPEST_DIR:-$(pwd)}
+export KEYSTONE_SERVICE_PROTOCOL=${KEYSTONE_SERVICE_PROTOCOL:-http}
+export KEYSTONE_SERVICE_HOST=${KEYSTONE_SERVICE_HOST:-127.0.0.1}
+export OS_AUTH_URL=http://${KEYSTONE_SERVICE_HOST}:5000/v2.0/
+export PUBLIC_NETWORK_NAME=${PUBLIC_NETWORK_NAME:-public_net}
+export PUBLIC_NETWORK_RI_FQ_NAME=${PUBLIC_NETWORK_RI_FQ_NAME:-"default-domain:$OS_TENANT_NAME:$PUBLIC_NETWORK_NAME:$PUBLIC_NETWORK_NAME"}
+export PUBLIC_NETWORK_RT=${PUBLIC_NETWORK_RT:-10003}
+export ROUTER_ASN=${ROUTER_ASN:-64510}
+export API_SERVER_IP=${API_SERVER_IP:-127.0.0.1}
+export API_SERVER_HOST_USER=${API_SERVER_HOST_USER:-root}
+export API_SERVER_HOST_PASSWORD=${API_SERVER_HOST_PASSWORD:-c0ntrail123}
+export PUBLIC_NETWORK_SUBNET=${PUBLIC_NETWORK_SUBNET:-10.1.1.0/24}
+export HTTP_IMAGE_PATH=${HTTP_IMAGE_PATH:-http://10.84.5.120/images/cirros/cirros-0.3.1-x86_64-disk.img}
+
+FLAVOR_ID=7
+SERVICE_HOST=$KEYSTONE_SERVICE_HOST
+
+TEMPEST_CONFIG_DIR=${TEMPEST_CONFIG_DIR:-$TEMPEST_DIR/etc}
+TEMPEST_CONFIG=$TEMPEST_CONFIG_DIR/tempest.conf
+
+source $TEMPEST_DIR/contrail/functions
+cd ${TEMPEST_DIR:-$(pwd)}
+
+cp $TEMPEST_DIR/etc/tempest.conf.sample $TEMPEST_CONFIG
+password=${ADMIN_PASSWORD:-contrail123}
+ALT_USERNAME=${ALT_USERNAME:-alt_demo}
+ALT_TENANT_NAME=${ALT_TENANT_NAME:-alt_demo}
+USERNAME="demo"
+PASSWORD=$OS_PASSWORD
+TENANT_NAME="demo"
+
+ADMIN_USERNAME="admin"
+ADMIN_PASSWORD=$OS_PASSWORD
+ADMIN_TENANT_NAME="admin"
+
+# ADD GLANCE IMAGE 
+# set image_uuid and image_uuid_alt
+image_uuid=`get_image_id $KEYSTONE_SERVICE_HOST "cirros" $HTTP_IMAGE_PATH`
+image_uuid_alt=`get_image_id $KEYSTONE_SERVICE_HOST "cirros" $HTTP_IMAGE_PATH`
+echo "Image id $image_uuid"
+
+# Create tenant, user and public network
+create_project $ALT_TENANT_NAME
+create_user $ALT_USERNAME $password $ALT_TENANT_NAME "member"
+create_public_network $PUBLIC_NETWORK_NAME $PUBLIC_NETWORK_SUBNET
+create_flavor $FLAVOR_ID
+
+#DASHBOARD
+iniset $TEMPEST_CONFIG dashboard dashboard_url "http://$SERVICE_HOST/"
+iniset $TEMPEST_CONFIG dashboard login_url "http://$SERVICE_HOST/auth/login/"
+
+##iniset $TEMPEST_CONFIG DEFAULT debug True
+
+#IDENTITY
+iniset $TEMPEST_CONFIG identity uri "$KEYSTONE_SERVICE_PROTOCOL://$KEYSTONE_SERVICE_HOST:5000/v2.0/"
+iniset $TEMPEST_CONFIG identity uri_v3 "$KEYSTONE_SERVICE_PROTOCOL://$KEYSTONE_SERVICE_HOST:5000/v3/"
+iniset $TEMPEST_CONFIG identity username $USERNAME
+iniset $TEMPEST_CONFIG identity password $PASSWORD
+iniset $TEMPEST_CONFIG identity tenant_name $TENANT_NAME
+
+iniset $TEMPEST_CONFIG identity alt_username $ALT_USERNAME
+iniset $TEMPEST_CONFIG identity alt_password $password
+iniset $TEMPEST_CONFIG identity alt_tenant_name $ALT_TENANT_NAME
+
+#AUTH
+iniset $TEMPEST_CONFIG auth admin_username $ADMIN_USERNAME
+iniset $TEMPEST_CONFIG auth admin_password $ADMIN_PASSWORD
+iniset $TEMPEST_CONFIG auth admin_project_name $ADMIN_TENANT_NAME
+iniset $TEMPEST_CONFIG auth use_dynamic_credentials True
+
+iniset $TEMPEST_CONFIG image http_image $HTTP_IMAGE_PATH
+
+#COMPUTE 
+public_network_id=$(${with_venv} neutron net-list | grep $PUBLIC_NETWORK_NAME | \
+            awk '{print $2}')
+
+iniset $TEMPEST_CONFIG compute ssh_user ${DEFAULT_INSTANCE_USER:-cirros}
+iniset $TEMPEST_CONFIG compute image_ref $image_uuid
+iniset $TEMPEST_CONFIG validation image_ssh_user ${DEFAULT_INSTANCE_USER:-cirros}
+iniset $TEMPEST_CONFIG validation image_ssh_password ${DEFAULT_INSTANCE_PASSWORD:-cubswin:)}
+iniset $TEMPEST_CONFIG compute flavor_ref $FLAVOR_ID
+iniset $TEMPEST_CONFIG compute image_ref_alt $image_uuid_alt
+iniset $TEMPEST_CONFIG compute image_alt_ssh_user ${DEFAULT_INSTANCE_USER:-cirros}
+iniset $TEMPEST_CONFIG compute image_alt_ssh_password ${DEFAULT_INSTANCE_PASSWORD:-cubswin:)}
+iniset $TEMPEST_CONFIG compute allow_tenant_isolation ${TENANT_ISOLATION:-false}
+iniset $TEMPEST_CONFIG network public_network_id "$public_network_id"
+
+# Disable IPv6 tests
+iniset $TEMPEST_CONFIG network-feature-enabled ipv6 false
+
+iniset $TEMPEST_CONFIG service_available "neutron" "True"
+iniset $TEMPEST_CONFIG service_available "cinder" "False"
+iniset $TEMPEST_CONFIG service_available "swift" "False"
+iniset $TEMPEST_CONFIG service_available "heat" "False"
+
+
---
+++ run_contrail_tempest.sh
@@ -0,0 +1,152 @@
+#!/usr/bin/env bash
+
+function usage {
+  echo "Usage: $0 [OPTION]..."
+  echo "Run Tempest test suite"
+  echo ""
+  echo "  -V, --virtual-env        Always use virtualenv.  Install automatically if not present"
+  echo "  -N, --no-virtual-env     Don't use virtualenv.  Run tests in local environment"
+  echo "  -n, --no-site-packages   Isolate the virtualenv from the global Python environment"
+  echo "  -f, --force              Force a clean re-build of the virtual environment. Useful when dependencies have been added."
+  echo "  -u, --update             Update the virtual environment with any newer package versions"
+  echo "  -s, --smoke              Only run smoke tests"
+  echo "  -t, --serial             Run testr serially"
+  echo "  -C, --config             Config file location"
+  echo "  -h, --help               Print this usage message"
+  echo "  -d, --debug              Run tests with testtools instead of testr. This allows you to use PDB"
+  echo "  -r, --result-xml         Path of Junitxml report to be generated"
+  echo "  -p, --populate-config         Populate config file and init contrail environment"
+  echo "  -- [TESTROPTIONS]        After the first '--' you can pass arbitrary arguments to testr "
+}
+
+testrargs=""
+venv=${VENV:-.venv}
+with_venv=tools/with_venv.sh
+serial=0
+always_venv=0
+never_venv=0
+no_site_packages=0
+debug=0
+force=0
+coverage=0
+wrapper=""
+config_file=""
+update=0
+result_xml="result.xml"
+populate_config=0
+
+if ! options=$(getopt -o VNnfusthdC:pr: -l virtual-env,no-virtual-env,no-site-packages,force,update,smoke,serial,help,debug,config:,populate-config,result-xml: -- "$@")
+then
+    # parse error
+    usage
+    exit 1
+fi
+
+eval set -- $options
+first_uu=yes
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -h|--help) usage; exit;;
+    -V|--virtual-env) always_venv=1; never_venv=0;;
+    -N|--no-virtual-env) always_venv=0; never_venv=1;;
+    -n|--no-site-packages) no_site_packages=1;;
+    -f|--force) force=1;;
+    -u|--update) update=1;;
+    -d|--debug) debug=1;;
+    -C|--config) config_file=$2; shift;;
+    -s|--smoke) testrargs+="smoke";;
+    -p|--populate-config) populate_config=1;;
+    -r|--result-xml) result_xml=$2; shift;;
+    -t|--serial) serial=1;;
+    --) [ "yes" == "$first_uu" ] || testrargs="$testrargs $1"; first_uu=no  ;;
+    *) testrargs="$testrargs $1";;
+  esac
+  shift
+done
+
+if [ -n "$config_file" ]; then
+    config_file=`readlink -f "$config_file"`
+    export TEMPEST_CONFIG_DIR=`dirname "$config_file"`
+    export TEMPEST_CONFIG=`basename "$config_file"`
+fi
+
+cd `dirname "$0"`
+
+if [ $no_site_packages -eq 1 ]; then
+  installvenvopts="--no-site-packages"
+fi
+
+function testr_init {
+  if [ ! -d .testrepository ]; then
+      ${wrapper} testr init
+  fi
+}
+
+function run_tests {
+
+  echo -e "Run tests"
+  testr_init
+  ${wrapper} find . -type f -name "*.pyc" -delete
+  export OS_TEST_PATH=./tempest/test_discover
+  if [ $debug -eq 1 ]; then
+      if [ "$testrargs" = "" ]; then
+           testrargs="discover ./tempest/test_discover"
+      fi
+      ${wrapper} python -m testtools.run $testrargs
+      return $?
+  fi
+
+  if [ $serial -eq 1 ]; then
+      echo Executing $testrargs with $wrapper
+      ${wrapper} testr run --subunit $testrargs | ${wrapper} subunit2junitxml -f -o $result_xml
+  else
+      ${wrapper} testr run --parallel --subunit $testrargs | ${wrapper} subunit2junitxml -f -o $result_xml
+  fi
+}
+
+if [ $never_venv -eq 0 ]
+then
+  echo -e "Setting up virtual env"
+  # Remove the virtual environment if --force used
+  if [ $force -eq 1 ]; then
+    echo "Cleaning virtualenv..."
+    rm -rf ${venv}
+  fi
+  if [ $update -eq 1 ]; then
+      echo "Updating virtualenv..."
+      virtualenv $installvenvopts $venv
+      $venv/bin/pip install -U -r requirements.txt
+  fi
+  if [ -e ${venv} ]; then
+    echo -e "env is already available"
+    wrapper="${with_venv}"
+  else
+    if [ $always_venv -eq 1 ]; then
+      echo -e "Automatically installing the virtualenv"
+      # Automatically install the virtualenv
+      virtualenv $installvenvopts $venv
+      wrapper="${with_venv}"
+      ${wrapper} pip install -U -r requirements.txt
+    else
+      echo -e "No virtual environment found...create one? (Y/n) \c"
+      read use_ve
+      if [ "x$use_ve" = "xY" -o "x$use_ve" = "x" -o "x$use_ve" = "xy" ]; then
+        # Install the virtualenv and run the test suite in it
+        virtualenv $installvenvopts $venv
+        wrapper=${with_venv}
+        ${wrapper} pip install -U -r requirements.txt
+      fi
+    fi
+  fi
+fi
+
+if [ $populate_config -eq 1 ]; then
+   (unset http_proxy && ./contrail/contrail-tempest-init.sh)
+fi
+
+##apply_patches
+(unset http_proxy && run_tests)
+retval=$?
+
+
+exit $retval
